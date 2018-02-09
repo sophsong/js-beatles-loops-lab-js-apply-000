67115844223a3ba828d87f77c8a35db6e11e53b7
Sophia Song <sophia.song@tufts.edu>
2018-02-09 21:45:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ function theBeatlesPlay(players, instruments) {
  }
  
  function johnLennonFacts() {
-     var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice", "foo", "bar"];
+     var facts = 
+     ["He was the last Beatle to learn to drive", 
+     "He was never a vegetarian", 
+     "He was a choir boy and boy scout", 
+     "He hated the sound of his own voice"];
      var newFacts = [];
      var i = 0;
      while (i < facts.length) {
